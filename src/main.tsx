db0f751733229b7a5d01e45a8a02c1f8b0774ebf
oziriz31 <yvanakoo02@outlook.com>
2025-07-10 19:16:21 +0400

    cleaning to deploy

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import {CartProvider} from "./Contexts/CartContext.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
